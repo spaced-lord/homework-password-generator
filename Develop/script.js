83672cde724b114a187e21e593d2ecfc0e65a791
spaced-lord <zackarynoe@gmail.com>
2020-10-12 17:26:40 -0600

    Variable declared, attempts at functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase = "lower case letters"
-var uppercase = "upper case letters"
-var numeric = "0 through 9"
-var specialChar = "Special characters"
+
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+// var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// var lowercase = "abcdefghijklmnopqrstuvwxyz";
+// var numbers = "0123456789"
+// var symbols = "!@#$%^&*()+";
+
+var count = prompt("How many characters are needed in your new password (more than 8, less than 128)?"); 
+// var lower = confirm("Do you wish to use lowercase letters?"); 
+// var upper = confirm("Do you wish to use uppercase letters?");
+// var numbers = confirm("Do you wish to use numbers?");
+// var specialChar = confirm("Do you wish to use special characters (!,#,&,$, etc.)?");
 
 
 
 
-
-// Write password to the #password input
+// // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); 
+  for(var i = 0; i < characters.length; i++) {
+      password = password + count(Math.floor(Math.random * '') - 1)
+  }
+  
+    
+    
+  }
+
+  
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); {
+
+  }
+
+   
+
+  
+
+
+
+
+// console.log(password)
